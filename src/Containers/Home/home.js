7251a2e8733279c73cbe73b3d483696a53a95abb
MM794971 <mmiryala@albany.edu>
2021-10-13 15:27:31 -0400

    UI/UX tag changes for the home page and carousel

--- src/Containers/Home/home.js
+++ src/Containers/Home/home.js
@@ -19,30 +19,26 @@ const Home = () => {
             <div className="carousel-item carousel-image-1 active">
               <div className="container carousel-image-1">
                 <div className="carousel-caption d-none d-sm-block text-right mb-5">
-                  <h1 className="display-3">Heading One</h1>
+                  <h1 className="display-3">Are you an amateur cook looking to share your recipes with the world?</h1>
                   <p className="lead">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Iste, aperiam vel ullam deleniti reiciendis ratione quod
-                    aliquid inventore vero perspiciatis.
+                   ROT-ME-NOT is looking for you!
                   </p>
-                  <a href="#" className="btn btn-danger btn-lg">
+                  {/* <a href="#" className="btn btn-danger btn-lg">
                     Sign Up Now
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
             <div className="carousel-item carousel-image-2">
               <div className="container">
                 <div className="carousel-caption d-none d-sm-block mb-5">
-                  <h1 className="display-3">Heading Two</h1>
+                  <h1 className="display-3"></h1>
                   <p className="lead">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Iste, aperiam vel ullam deleniti reiciendis ratione quod
-                    aliquid inventore vero perspiciatis.
+                    Browse through our virtual cookbook to explore a plethora of cuisines from all over the planet
                   </p>
-                  <a href="#" className="btn btn-primary btn-lg">
+                  {/* <a href="#" className="btn btn-primary btn-lg">
                     Learn More
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
@@ -50,11 +46,9 @@ const Home = () => {
             <div className="carousel-item carousel-image-3">
               <div className="container">
                 <div className="carousel-caption d-none d-sm-block text-right mb-5">
-                  <h1 className="display-3">Heading Three</h1>
+                  <h1 className="display-3"></h1>
                   <p className="lead">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Iste, aperiam vel ullam deleniti reiciendis ratione quod
-                    aliquid inventore vero perspiciatis.
+                    ROT-ME-NOT has hand-picked expert chefs for guidance who strive to share their knowledge!
                   </p>
                   <a href="#" className="btn btn-success btn-lg">
                     Learn More
